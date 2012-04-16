a59a8498d063b53451a05e8e973109ea912b3c92
Anders Pearson <anders@columbia.edu>
2012-04-16 10:07:10 -0400

    version bump in setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="restclient",
-    version="0.10.1",
+    version="0.10.2",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="http://github.com/thraxil/restclient/",
